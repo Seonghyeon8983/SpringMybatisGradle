be6de66c1b5ae216ce69c874855e75895cff93b5
sungHyeon <qortjdgus8999@gmail.com>
2025-03-11 16:11:54 +0900

    first commit

---
+++ src/main/java/data/controller/SawonController.java
@@ -0,0 +1,18 @@
+package data.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class SawonController {
+	@GetMapping({"/","/list"})
+	public String sawonList() 
+	{
+		return "sawon/sawonlist";
+	}
+	
+	
+	
+}
