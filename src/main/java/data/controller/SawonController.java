67c84d06cda6c6fab5b59a48dcc14708137d7b05
sungHyeon <qortjdgus8999@gmail.com>
2025-03-13 17:17:52 +0900

    헤더푸터

--- src/main/java/data/controller/SawonController.java
+++ src/main/java/data/controller/SawonController.java
@@ -2,17 +2,18 @@ package data.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @Controller
 public class SawonController {
 	@GetMapping({"/","/list"})
-	public String sawonList() 
+	public String sawonList()
 	{
-		return "sawon/sawonlist";
+		return "sawon/sawonlist";		
 	}
 	
-	
-	
+	@GetMapping("/form")
+	public String sawonForm()
+	{
+		return "sawon/sawonform";		
+	}
 }
